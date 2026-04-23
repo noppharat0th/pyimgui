14360a6dfb08d49da8c7f330e57e0bc25eb1251e
Nopparatz1238 <uiavgcgccggv@gmail.com>
2026-04-23 23:06:56 +0700

    set window-size & option-window-flags

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from imgui_bundle import imgui as ImGui, immapp
 
 def gui():
+    style = ImGui.get_style()
     ImGui.style_colors_dark()
-    ImGui.show_demo_window()
+
+    style.window_rounding = 10.0
+
+    ImGui.set_next_window_size(ImGui.ImVec2(700, 500))
+    ImGui.begin("window", None, ImGui.WindowFlags_.no_title_bar | ImGui.WindowFlags_.no_resize)
+    ImGui.end()
 
 def render():
     immapp.run(gui_function=gui, window_title="Hello ImGui Bundle", window_size=(400, 300))
