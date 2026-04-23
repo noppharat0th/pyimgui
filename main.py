4c339217c844fa6a809178ba843643f54af007c1
Nopparatz1238 <uiavgcgccggv@gmail.com>
2026-04-23 22:15:02 +0700

    update structure

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from imgui_bundle import imgui as ImGui, immapp
 
 def gui():
+    ImGui.style_colors_dark()
     ImGui.show_demo_window()
 
-def config():
-    ImGui.style_colors_dark()
-
 def render():
-    config();
     immapp.run(gui_function=gui, window_title="Hello ImGui Bundle", window_size=(400, 300))
 
-render();
+if __name__ == "__main__":
+    render();
